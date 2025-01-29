c3357a7e218feb74073022f91216e0395ae344f3
nyacho04 <dnachod83@gmail.com>
2025-01-29 14:26:36 -0300

    classes and objects

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""square"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size):
+        self.__size = size
---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a class Rectangle"""
+
+
+class Rectangle:
+    """Empty class that defines a rectangle"""
+    pass
