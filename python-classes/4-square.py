326baf4f6e15eae8d4073a6323f1c8f855a40548
nyacho04 <dnachod83@gmail.com>
2025-02-02 13:44:07 -0300

    square classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,6 +10,7 @@ class Square:
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,5 +18,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,9 +7,11 @@ class Square:
 
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,8 +19,10 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
+
     def my_print(self):
         if self.size == 0:
             print()
