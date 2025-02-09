306f893a4a6b6a300cdb2e6a70d15fc73d19babb
nyacho04 <dnachod83@gmail.com>
2025-02-09 04:09:10 -0300

    pycodestyle fixes

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """module that defines a class MyList"""
-
-
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """rectangle"""
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """module that defines a class MyList"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """class"""
