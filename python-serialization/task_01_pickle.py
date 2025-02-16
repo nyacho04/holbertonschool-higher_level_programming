27b7d96f03d85bc4263066f5a679e10946848870
nyacho04 <dnachod83@gmail.com>
2025-02-16 18:27:19 -0300

    commit

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """ filename """
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """ file """
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception as e:
+            return None
+            
--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """ function """
+    try:
+
+        with open(csv_filename, mode='r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
+
+        with open('data.json', mode='w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+
+    except FileNotFoundError:
+        return False
+    except Exception as e:
+        return False
