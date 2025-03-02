997a022d7f3794a8a41a4dcd4ac9fc8d5f9457b3
nyacho04 <dnachod83@gmail.com>
2025-03-02 20:48:27 -0300

    commit

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,10 +31,10 @@ class Server(srv.BaseHTTPRequestHandler):
             self.wfile.write(jout.encode('utf-8'))
         else:
             self.send_response(404)
-            self.send_header('Content-Type', 'application/json')
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            jout = json.dumps({"error": "Endpoint not found"})
-            self.wfile.write(jout.encode('utf-8'))
+            self.wfile.write(b"Endpoint not found\n")
+            
             
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), Server) as httpd:
