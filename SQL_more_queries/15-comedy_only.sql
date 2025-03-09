f8e3bb44d56037260f183213f0a75d04d7a04f07
nyacho04 <dnachod83@gmail.com>
2025-03-09 08:25:08 -0300

    commit

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN genres ON tv_show_genres.genre_id = genres.id
-WHERE genres.name = 'Comedy'
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
