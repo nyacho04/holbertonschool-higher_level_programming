e9847066840f7d61c00c250e1fc56298cc9e37b0
nyacho04 <dnachod83@gmail.com>
2025-03-16 21:15:34 -0300

    commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(state_name_searched)
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' "
+             "ORDER BY id ASC".format(state_name_searched))
     cursor.execute(query)
 
     rows = cursor.fetchall()
